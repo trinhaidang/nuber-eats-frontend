403c20228e17f28dd16c700c4bc9dbf1f6023ece
trinh <trinh.dang@codestringers.com>
2021-08-08 14:11:11 +0700

    14.4 - apollo

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./styles/styles.css";
+import { ApolloProvider } from "@apollo/client";
+import { client } from './apollo';
 
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client} >
     <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
