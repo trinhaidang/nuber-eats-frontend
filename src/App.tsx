4ac3256b5901481bef70b53d68ed7442daa5e1ed
trinh <trinh.dang@codestringers.com>
2021-08-08 21:06:52 +0700

    15.2 - login form react hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import './App.css';
-import React from 'react';
 import { LoggedOutRouter } from './routers/logged-out-router';
 import { LoggedInRouter } from './routers/logged-in-router';
-import gql from 'graphql-tag';
-import { useQuery, useReactiveVar } from '@apollo/client';
+import { useReactiveVar } from '@apollo/client';
 import { isLoggedInVar } from './apollo';
 
 function App() {
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,16 +1,19 @@
-import React, { InputHTMLAttributes, useRef } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 
+interface IForm {
+    email: string;
+    password: string;
+}
 
 export const LoggedOutRouter = () => {
-    const { register, watch, handleSubmit, formState: { errors } } = useForm();
+    const { register, watch, handleSubmit, formState: { errors } } = useForm<IForm>();
     const onSubmit = () => {
         console.log(watch());
     };
     const onInvalid = () => {
         console.log("cant create account");
     };
-    console.log(errors);
     return (
         <div>
             <h1>Logged Out</h1>
@@ -19,6 +22,9 @@ export const LoggedOutRouter = () => {
                     <input
                         {...register("email", { required: "This is required", pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/, })}
                         name="email" type="email" placeholder="email" />
+                    {errors.email?.message && (<span className="font-bold text-red-600">{errors.email.message}</span>) }
+                    {errors.email?.type === "pattern" && (<span className="font-bold text-red-600">Only gmail allowed</span>) }
+                    
                 </div>
                 <div>
                     <input
