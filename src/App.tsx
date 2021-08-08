b93e5658e339a7e7a2537838964a28ebcea8d333
trinh <trinh.dang@codestringers.com>
2021-08-08 19:33:42 +0700

    15.0 - login logout btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import './App.css';
+import React from 'react';
 import { LoggedOutRouter } from './routers/logged-out-router';
+import { LoggedInRouter } from './routers/logged-in-router';
+import gql from 'graphql-tag';
+import { useQuery, useReactiveVar } from '@apollo/client';
+import { isLoggedInVar } from './apollo';
 
 function App() {
-  return <LoggedOutRouter />;
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />;
 }
 
 export default App;
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,3 +1,8 @@
 import React from "react";
+import { isLoggedInVar } from "../apollo";
 
-export const LoggedInRouter = () => <span>Logged In</span>;
+export const LoggedInRouter = () => 
+<div>
+    <h1>Logged In</h1>
+    <button onClick={() => isLoggedInVar(false)}>Click to Log Out</button>
+</div>
