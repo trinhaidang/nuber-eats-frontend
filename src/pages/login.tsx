1a3c3ed010b8afe265012bf75589ec45ea3e6835
trinh <trinh.dang@codestringers.com>
2021-08-09 16:22:06 +0700

    15.6 - mutation login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,15 +1,39 @@
+import { gql, useMutation } from "@apollo/client";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { FormError } from "../components/form-error";
+
+const LOGIN_MUTATION = gql`
+    mutation PotatoMutation($email:String!, $password:String!) {
+        login(input: {
+            email:$email,
+            password:$password
+        }) {
+            ok
+            token
+            error
+        }
+    }
+`;
 
 interface ILoginForm {
-    email?: string;
-    password?: string;
+    email: string;
+    password: string;
 }
 
 export const Login = () => {
 
     const { register, getValues, formState: { errors }, handleSubmit } = useForm<ILoginForm>();
-    const onSubmit = () => {};
+    const [loginMutation, {loading, error, data}] = useMutation(LOGIN_MUTATION);
+    const onSubmit = () => {
+        const {email, password} = getValues();
+        loginMutation({
+            variables: {
+                email,
+                password,
+            },
+        });
+    };
 
     return <div className=" h-screen flex items-center justify-center bg-gray-800 ">
         <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
@@ -24,12 +48,10 @@ export const Login = () => {
                     className="input"
                 />
                 {errors.email?.message && (
-                    <span className=" font-medium text-red-500">
-                        {errors.email?.message}
-                    </span>
+                    <FormError errorMessage={errors.email?.message} />
                 )}
                 <input 
-                    {...register("password", { required: "Password is required", minLength: 10 })} 
+                    {...register("password", { required: "Password is required"})} 
                     required
                     name="password" 
                     type="password" 
@@ -37,14 +59,10 @@ export const Login = () => {
                     className="input" 
                 />
                 {errors.password?.type === "minLength" && (
-                    <span className=" font-medium text-red-500">
-                        Password must be more than 10 chars.
-                    </span>
+                    <FormError errorMessage="Password must be more than 10 chars." />
                 )}
                 {errors.password?.message && (
-                    <span className=" font-medium text-red-500">
-                        {errors.password?.message}
-                    </span>
+                    <FormError errorMessage={errors.password?.message} />
                 )}
                 <button className="btn mt-3">Log In</button>
             </form>
