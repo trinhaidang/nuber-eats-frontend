e1b7ea59784124eac8c00d0bc40e90c4c6aabb63
trinh <trinh.dang@codestringers.com>
2021-08-11 19:28:29 +0700

    17.1 categories design

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -8,11 +8,48 @@ export const Restaurants = () => {
         RESTAURANTS_QUERY,
         {
             variables: {
-                input: { page: 1}
+                input: { page: 1 }
             }
         }
     );
+    const images_folder = "/images/";
 
     console.log(data);
-    return (<h1>Restaurants</h1>);
+    return (
+        <div>
+            {/* Search Banner */}
+            <form name="restaurants-search-form" className="bg-gray-800 w-full py-40 flex items-center justify-center" >
+                <input
+                    type="Search"
+                    className="input rounded-md border-0 w-3/12"
+                    placeholder="Search restaurants..."
+                />                
+            </form>
+
+            {!loading &&
+                <div className="max-w-screen-2xl mx-auto mt-8">
+                    {/* Categories list */}
+                    <div className="flex justify-around max-w-sm mx-auto">
+                        {data?.allCategories.categories?.map((category) => {
+                            const img = images_folder + (category.coverImg ? category.coverImg : "categories/default.png");
+                            return (
+                                <div className="flex flex-col items-center cursor-pointer">
+                                <div
+                                    className="w-14 h-14 bg-cover hover:bg-gray-100 rounded-full "
+                                    style={{backgroundImage: `url(${img})` ,}}
+                                >
+                                </div>
+                                    <span className="mt-1 text-sm text-center font-medium">{category.name[0].toUpperCase()}{category.name.slice(1)}</span>
+                                </div>
+                            );
+                        })}
+                    </div>
+                    {/* Restaurants list */}
+                    <div>
+
+                    </div>
+                </div>
+            }
+        </div>
+    );
 };
