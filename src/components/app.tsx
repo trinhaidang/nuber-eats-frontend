ebd68197b0c6a4bd622dec9432f122a7cf962bc0
trinh <trinh.dang@codestringers.com>
2021-08-12 19:24:12 +0700

    18 - setup test

---
+++ src/components/app.tsx
@@ -0,0 +1,11 @@
+import '../App.css'
+import { LoggedOutRouter } from '../routers/logged-out-router';
+import { LoggedInRouter } from '../routers/logged-in-router';
+import { useReactiveVar } from '@apollo/client';
+import { isLoggedInVar } from '../apollo';
+
+export const App = () => {
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />;
+}
+
